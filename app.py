dca895ee289a3b78b5b42d0bc2a74b58fee397af
Nicholas <phamn1000@gmail.com>
2023-07-12 15:00:20 -0400

    Added unique parameter for usernames.

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ db.init_app(app)
 
 class User(db.Model, UserMixin):
 	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(20), nullable=False)
+	username = db.Column(db.String(20), nullable=False, unique=True)
 	password = db.Column(db.String(80), nullable=False)
 
 # Routes
